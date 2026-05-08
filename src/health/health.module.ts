14044b10ec4673e9216bbff5de477a7eda631bde
Maciej Chrzanowski <mchrzanol22@gmail.com>
2026-05-08 08:58:44 +0200

    feat(sheet): add endpoint to sync sheet with new headers + add these new headers

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HealthController } from './health.controller';
 import { CronModule } from '../cron/cron.module';
+import { SheetsModule } from '../sheets/sheets.module';
 
 @Module({
-  imports: [CronModule],
+  imports: [CronModule, SheetsModule],
   controllers: [HealthController],
 })
 export class HealthModule {}
